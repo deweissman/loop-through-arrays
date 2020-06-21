d9cd76ee02a717d9f7b7e8730b5060a9564e81fc
David Weissman <davidweissman@me.com>
2020-06-21 16:39:19 -0400

    Adding files for 'Loop Through Arrays' lesson

---
+++ js/array.js
@@ -0,0 +1,9 @@
+const planets = ["Mercury", "Venus", "Earth", "Mars"];
+
+console.log(planets[0]);
+console.log(planets[1]);
+console.log(planets[2]);
+console.log(planets[3]);
+console.log(planets[4]);
+
+console.log(planets.length);
